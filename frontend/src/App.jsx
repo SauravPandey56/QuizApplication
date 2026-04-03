85084e5b0bc35ac845bd72f40099dc4ea4ea4974
Saurav Pandey <pandeysaurav108@gmail.com>
2026-04-03 10:10:34 +0530

    tailwind css install successfully

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,12 @@
-import React from "react";
+import React from 'react'
 
 const App = () =>{
     return(
         <div>
-            <h1 classNamem ="text-3xl font-bold underline" >
+            <h1 className ="text-3xl font-bold underline" >
                 Hello World !
             </h1>
         </div>
     )
 }
-
 export default App
